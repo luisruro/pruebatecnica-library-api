d02cc8161be3249be60e2c30dae91a401b0312d8
luisruro <lerubioromero@gmail.com>
2024-09-28 21:14:54 -0500

    feat: login.dto.ts was created

---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,16 @@
+import { Transform } from "class-transformer";
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword } from "class-validator";
+
+export class LoginDto {
+
+    @IsNotEmpty({ message: 'Email is required and must be a valid email address' })
+    @IsString()
+    @IsEmail({}, { message: 'Please provide a valid email address' })
+    emil: string;
+
+    @IsNotEmpty({ message: 'Password is required and must be a valid password: at least 8 characters, 1 upper case, at least one digit from (0-9) and at least one special character' })
+    @IsString()
+    @IsStrongPassword({}, { message: 'Please provide a valid password: at least 8 characters, at least 1 upper case, at least one digit from (0-9) and at least one special character' })
+    @Transform(({ value }) => value.trim())
+    password: string;
+}
