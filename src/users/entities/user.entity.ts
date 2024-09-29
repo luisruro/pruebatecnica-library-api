ced7d32bd2f15752b5225ceba5605120f9f7fcd9
luisruro <lerubioromero@gmail.com>
2024-09-28 20:33:58 -0500

    user.entity.ts was created and added into the module and exporting UserService

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('Users')
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ length: 500 })
+    name: string;
+
+    @Column({ unique: true, nullable: false })
+    email: string;
+
+    @Column({ nullable: false })
+    password: string;
+
+    @Column({ default: "user" })
+    rol: string;
+
+    @CreateDateColumn()
+    creationDate: Date;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+}
