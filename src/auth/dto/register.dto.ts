715b3d55e98266c43e2cc730f3e8d7911393a834
luisruro <lerubioromero@gmail.com>
2024-09-28 21:12:51 -0500

    feat: register.dto.ts was created

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,7 +1,7 @@
 import { Transform } from "class-transformer";
 import { IsEmail, IsNotEmpty, IsString, IsStrongPassword, MinLength } from "class-validator";
 
-export class CreateUserDto {
+export class RegisterDto {
 
     @IsNotEmpty({message: 'Name is required and must be a string'})
     @IsString()
@@ -11,7 +11,7 @@ export class CreateUserDto {
     @IsNotEmpty({message: 'Email is required and must be a valid email address'})
     @IsString()
     @IsEmail({},{ message: 'Please provide a valid email address' })
-    email: string;
+    emil:string;
 
     @IsNotEmpty({message: 'Password is required and must be a valid password: at least 8 characters, 1 upper case, at least one digit from (0-9) and at least one special character'})
     @IsString()
